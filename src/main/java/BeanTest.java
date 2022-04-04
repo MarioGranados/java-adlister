41cc864409d043223177c74b24bd3aad85162036
Mario Granados <mario.alberto.granados1@gmail.com>
2022-04-04 10:30:54 -0500

    javaAdlisterEx

--- src/main/java/BeanTest.java
+++ src/main/java/BeanTest.java
@@ -1,2 +1,12 @@
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class BeanTest {
+    public static void main(String[] args) {
+        Quote quote = new Quote();
+        Author author = new Author();
+        Album album = new Album();
+
+        ArrayList<Quote> quotes = new ArrayList<>();
+    }
 }
--- src/main/java/Quote.java
+++ src/main/java/Quote.java
@@ -1,2 +1,33 @@
-public class Quote {
+import java.io.Serializable;
+
+public class Quote implements Serializable {
+    private long id;
+    private String content;
+    private long authorId;
+
+    public Quote() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(long authorId) {
+        this.authorId = authorId;
+    }
 }
