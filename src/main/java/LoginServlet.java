a7d9f387b7e9f335faf1ed98c55e27d768159414
Mario Granados <mario.alberto.granados1@gmail.com>
2022-04-05 14:50:14 -0500

    finished

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,11 +3,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
+        }
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
@@ -15,11 +20,13 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
+        request.getSession().setAttribute("user", false);
 
         if (validAttempt) {
-            response.sendRedirect("/profile");
+            request.getSession().setAttribute("user", true);
+            response.sendRedirect("profile");
         } else {
-            response.sendRedirect("/login");
+            response.sendRedirect("login");
         }
     }
 }
